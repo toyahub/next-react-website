bdeb7e094a932dee8cbd63033311ad31f21362e5
toyahub <103926108+toyahub@users.noreply.github.com>
2022-12-09 16:31:39 +0900

    1209
    aa

---
+++ pages/about.jsx
@@ -0,0 +1,18 @@
+import ero from 'components/hero'
+export default function Home() {
+    return (
+        <>
+            <header>HEADER</header>
+
+            <main>
+                <div>
+                    <h1>CUBE</h1>
+                    <p>アウトプットしていくサイト</p>
+                </div>
+            </main>
+
+            <footer>FOOTER</footer>
+        </>
+
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import layout from 'components/layout'
+import hero from '../components/hero'
+
+
+export default function Home() {
+  return <Hero />
+}
+ 
